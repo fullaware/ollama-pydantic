4e1a45d6573b2fc9247c1dbdf98e405d7001cef2
Full Aware <fullaware@users.noreply.github.com>
2025-03-08 20:32:07 -0500

    remove build

--- build.sh
+++
@@ -1,5 +0,0 @@
-docker build . -t fullaware/ollama-pydantic:latest
-docker push fullaware/ollama-pydantic:latest
-
-helm upgrade ollama-pydantic ./ollama-pydantic/ -n ollama-pydantic --create-namespace
-kubectl rollout restart deploy ollama-pydantic-app -n ollama-pydantic
